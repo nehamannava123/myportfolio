d9de16c8eece00f77d6a275ba8badda4dd71d033
nehamannava123 <nehamannava123@gmail.com>
2026-06-04 18:34:27 +0530

    Initial portfolio website

---
+++ Script.js
@@ -0,0 +1,17 @@
+var typed = new Typed(".typing", {
+
+strings: [
+
+"AI Enthusiast",
+"Web Developer",
+"Machine Learning Learner",
+"Java Developer",
+"Problem Solver"
+
+],
+
+typeSpeed: 100,
+backSpeed: 50,
+loop: true
+
+});
